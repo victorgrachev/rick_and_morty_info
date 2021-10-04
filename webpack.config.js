d6db4ceeee439bcb1cafe8313b76639761c6e874
Victor Grachev <v.grachev@cft.ru>
2021-10-04 13:33:18 +0700

    Создание конфигурации сборщика

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OptimizeCssAssetPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const port = process.env.PORT ?? 8000;
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: 'all',
+    },
+  };
+
+  if (isProd) {
+    config.minimizer = [
+      new TerserWebpackPlugin(),
+      new OptimizeCssAssetPlugin(),
+    ];
+  }
+
+  return config;
+};
+
+module.exports = {
+  entry: {
+    main: path.join(__dirname, 'src', 'entry', 'index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name].[hash].js',
+  },
+  resolve: {
+    alias: {
+      '@module': path.join(__dirname, 'src', 'module'),
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+      minify: {
+        collapseWhitespace: isProd,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({ filename: '[name].[hash].css' }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    port: port,
+    hot: 'only',
+  },
+  devtool: isDev ? 'source-map' : false,
+  optimization: optimization(),
+};
