2e3bd9d248bd976f4a31701a6a9ddbe9d6a1dbb2
Victor Grachev <v.grachev@cft.ru>
2021-10-04 20:24:23 +0700

    Убран стиль шрифта

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 
 function Home() {
-  const style = { color: 'white' };
   return (
-    <div className="container p-0" style={style}>
+    <div className="container p-0">
       <h1 className="display-4 Light">Информация о сериале Rick and Morty</h1>
       <p className="lead">
         Версия приложения <strong>1.0.1</strong>
