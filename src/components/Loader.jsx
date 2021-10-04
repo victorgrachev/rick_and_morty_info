226ce55ed3b18821c77e3f8ea8ecb8bb37781187
Victor Grachev <v.grachev@cft.ru>
2021-10-04 20:23:38 +0700

    Создание компонента Loader

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Loader() {
+  return (
+    <div className="text-center">
+      <div className="spinner-border" role="status" />
+    </div>
+  );
+}
+
+export default Loader;
