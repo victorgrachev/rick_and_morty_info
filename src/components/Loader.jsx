b8250605bac54e76550870807c9488ce858ca70a
Victor Grachev <v.grachev@cft.ru>
2021-10-05 19:48:59 +0700

    Добавление отступов

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Loader() {
   return (
-    <div className="text-center">
+    <div className="text-center col-4 m-5">
       <div className="spinner-border" role="status" />
     </div>
   );
