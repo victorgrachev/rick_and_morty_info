adb3cd4d5577b72e4e3b94ed520a949d5e53cd0d
Victor Grachev <v.grachev@cft.ru>
2021-10-06 15:33:26 +0700

    Добавлено центрирование

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Loader() {
   return (
-    <div className="text-center col-4 m-5">
+    <div className="d-flex justify-content-center w-100 col-4 m-5">
       <div className="spinner-border" role="status" />
     </div>
   );
